57a048ce8ced050d6f1c9495194bd1722a537637
Raswanth8 <raswanth8901@gmail.com>
2022-09-24 22:08:33 +0530

    Added Styles

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -5,11 +5,13 @@ export default function Hero() {
     // typography in the center
     <div className="flex flex-col items-center ">
       <br />
-      <p className="text-2xl">The Ultimate Inventory Management Platform</p>
+      <p className="text-2xl font-inter">
+        The Ultimate Inventory Management Platform
+      </p>
       <br />
-      <h1 className="text-6xl font-bold">InviChat</h1>
+      <h1 className="text-6xl font-bold font-poppins">InviChat</h1>
       <br />
-      <button className="text-white bg-blue-500 btn hover:bg-blue-700">
+      <button className="text-white bg-blue-500 btn hover:bg-blue-700 font-poppins">
         Try our WhatsApp Bot! ⮕
       </button>
     </div>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,31 +4,33 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-white-800 p-6 shadow-none rounded-lg">
       <div className="flex items-center flex-shrink-0 text-black mr-6">
-        <span className="font-semibold text-xl tracking-tight">InviChat</span>
+        <span className="font-semibold text-xl tracking-tight font-poppins">
+          InviChat
+        </span>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4 font-poppins"
           >
             Home
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4 font-poppins"
           >
             About
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-400 hover:text-black mr-4 font-poppins"
           >
             Signup
           </a>
         </div>
         <div>
-          <button class="text-white bg-blue-500 btn hover:bg-blue-700">
+          <button class="text-white bg-blue-500 btn hover:bg-blue-700 font-poppins">
             Try it!
           </button>
         </div>
