629e7004db7142b44d6071c4d5653577c9c3793f
Raswanth8 <raswanth8901@gmail.com>
2022-09-24 12:07:42 +0530

    Added Hero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Hero() {
-  return <></>;
+  return (
+    // typography in the center
+    <div className="flex flex-col items-center ">
+      <br />
+      <p className="text-2xl">The Ultimate Inventory Management Platform</p>
+      <br />
+      <h1 className="text-6xl font-bold">InviChat</h1>
+      <br />
+      <button className="text-white bg-blue-500 btn hover:bg-blue-700">
+        Try our WhatsApp Bot! ⮕
+      </button>
+    </div>
+  );
 }
