b8d6318d7bcbddf11eefea70e0b546d07e491401
Raswanth8 <raswanth8901@gmail.com>
2022-09-25 21:14:11 +0530

    Added Styles

--- frontend/src/components/Partners.js
+++ frontend/src/components/Partners.js
@@ -8,10 +8,10 @@ export default function Partners() {
         <text className=" italic text-blue-500">Partner</text>&nbsp; Websites
       </p>
       <br />
-      <div className="grid grid-cols-3 gap-3 ">
+      <div className="grid grid-cols-2 gap-3 ">
         <div>
           <img
-            src="https://cdn.shortpixel.ai/spai/w_748+q_lossy+ret_img+to_webp/https://tinuiti.com/wp-content/uploads/legacysitecontent/cpcs/posts_01/2018/04/10095553/Amazon-seller-central.png"
+            src="https://pngimg.com/uploads/amazon/amazon_PNG13.png"
             width={300}
             height={200}
             className="object-contain"
@@ -23,13 +23,6 @@ export default function Partners() {
             width={300}
             height={200}
             className="object-contain"
-          />
-        </div>
-        <div>
-          <img
-            src="https://1000logos.net/wp-content/uploads/2022/08/Myntra-Logo.png"
-            width={300}
-            height={200}
           />
         </div>
       </div>
