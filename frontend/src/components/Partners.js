ea7c050f3efab87aedbab037a74571b95ab3606b
Raswanth8 <raswanth8901@gmail.com>
2022-09-25 21:02:46 +0530

    Added Styles

--- frontend/src/components/Partners.js
+++ frontend/src/components/Partners.js
@@ -8,26 +8,28 @@ export default function Partners() {
         <text className=" italic text-blue-500">Partner</text>&nbsp; Websites
       </p>
       <br />
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-3 gap-3 ">
         <div>
           <img
             src="https://cdn.shortpixel.ai/spai/w_748+q_lossy+ret_img+to_webp/https://tinuiti.com/wp-content/uploads/legacysitecontent/cpcs/posts_01/2018/04/10095553/Amazon-seller-central.png"
-            width={200}
-            height={150}
+            width={300}
+            height={200}
+            className="object-contain"
           />
         </div>
         <div>
           <img
             src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-logo-transparent-vector-3.png"
-            width={250}
-            height={150}
+            width={300}
+            height={200}
+            className="object-contain"
           />
         </div>
         <div>
           <img
             src="https://1000logos.net/wp-content/uploads/2022/08/Myntra-Logo.png"
-            width={250}
-            height={150}
+            width={300}
+            height={200}
           />
         </div>
       </div>
