a175be67de376b373e57dac49ed062fc4e4f207d
Venkata Sridhar Sai <kv.sridharsai@gmail.com>
2022-12-08 09:16:31 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     function fetchProducts() {
       axios
-        .get("https://amazon-sambhav-website-server.herokuapp.com/getProducts")
+        .get("https://invichat-website-server.azurewebsites.net/getProducts")
         .then((response) => {
           console.log(response.data);
           setData(response.data);
